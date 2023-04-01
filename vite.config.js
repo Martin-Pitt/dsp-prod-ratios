fff3de807e66a06660e02d83895e597493124a5e
Martin Pitt <martin.r.pitt@gmail.com>
2023-04-01 22:36:00 +0100

    Add base to config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()]
-})
+	plugins: [preact()],
+	base: '/dsp-prod-ratios/',
+});

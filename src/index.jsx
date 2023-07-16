5871b708e99afd63a21d6c58abbae093cc1eed40
Martin Pitt <martin.r.pitt@gmail.com>
2023-07-16 08:21:50 +0100

    try 404 redirect for SPAs

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,12 @@
 import { render } from 'preact'
 import { App } from './app.jsx'
 
+
+if(window.location.search.startsWith('?[') && window.location.search.endsWith(']'))
+{
+    let [pathname, search, hash] = JSON.parse(window.location.search.slice(1)).map(part => decodeURIComponent(part));
+    history.replaceState('', '', pathname + '?' + search + '#' + hash);
+}
+
+
 render(<App/>, document.getElementById('app'));

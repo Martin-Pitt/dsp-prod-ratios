a8a173df7b8c9ff02dfb1424b8491da6c7b39a4b
Martin Pitt <martin.r.pitt@gmail.com>
2023-07-18 03:25:04 +0100

    Add view transition api to routing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,16 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import iconGithub from '../images/github-mark-white.svg';
 import logo from '../images/logo.svg';
 
 export default function Header(props) {
+	const navigate = useNavigate();
+	const viewNavigate = (newRoute) => {
+		if(!document.startViewTransition) return navigate(newRoute);
+		return document.startViewTransition(() => {
+			navigate(newRoute);
+		});
+	};
+	
 	return (
 		<header class="app-header">
 			<h1 class="title">
@@ -10,9 +18,9 @@ export default function Header(props) {
 				DSP Production Ratio Calculator
 			</h1>
 			<div class="links">
-				<NavLink className="link" to="/calculator">Calculator</NavLink>
-				<NavLink className="link" to="/research">Research</NavLink>
-				<NavLink className="link" to="/reference">Reference</NavLink>
+				<NavLink className="link" to="/calculator" onClick={(event) => { event.preventDefault(); viewNavigate('/calculator') }}>Calculator</NavLink>
+				<NavLink className="link" to="/research" onClick={(event) => { event.preventDefault(); viewNavigate('/research') }}>Research</NavLink>
+				<NavLink className="link" to="/reference" onClick={(event) => { event.preventDefault(); viewNavigate('/reference') }}>Reference</NavLink>
 				{/* <NavLink className="link" to="/settings">Settings</NavLink> */}
 			</div>
 			<a class="github" target="_blank" href="https://github.com/Martin-Pitt/dsp-prod-ratios">

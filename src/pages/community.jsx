6fa6e13309042f5fdd11f870beee3ffb9fd19ad1
Martin Pitt <martin.r.pitt@gmail.com>
2023-07-29 09:06:52 +0100

    Bit more research on other tools

--- src/pages/community.jsx
+++ src/pages/community.jsx
@@ -6,7 +6,7 @@ export default function Community(props) {
 			<ul>
 				<li>
 					Official DSP Discord
-					<a target="_blank" href="https://discord.gg/dysonsphereprogram">https://discord.gg/dysonsphereprogram</a>
+					<a target="_blank" href="https://discord.com/servers/dyson-sphere-program-750553061369577492">https://discord.com/servers/dyson-sphere-program-750553061369577492</a>
 				</li>
 				<li>
 					Official DSP Subreddit
@@ -21,12 +21,52 @@ export default function Community(props) {
 					<a target="_blank" href="https://wiki.biligame.com/dsp/">https://wiki.biligame.com/dsp/</a>
 				</li>
 				<li>
+					Steam Community
+					<a target="_blank" href="https://steamcommunity.com/app/1366540/discussions">https://steamcommunity.com/app/1366540/discussions</a>
+				</li>
+				<li>
 					Dyson Shpere Blueprints
 					<a target="_blank" href="https://www.dysonsphereblueprints.com/">https://www.dysonsphereblueprints.com/</a>
 				</li>
 				<li>
 					Factoriolab
 					<a target="_blank" href="https://factoriolab.github.io/list?s=dsp&v=9">https://factoriolab.github.io/list?s=dsp&v=9</a>
+				</li>
+				<li>
+					lascyb's Circuit Diagram Generator
+					<a target="_blank" href="http://games.bxbon.com/index/dyson_sphere/production">http://games.bxbon.com/index/dyson_sphere/production</a>
+				</li>
+				<li>
+					DSP Quantization Tool (based on Svlik's)
+					<a target="_blank" href="https://gzh0821.github.io/DysonSphereProgramTool/">https://gzh0821.github.io/DysonSphereProgramTool/</a>
+				</li>
+				<li>
+					Eurydia's DSP Production Calculator
+					<a target="_blank" href="https://eurydia.github.io/dsp-calculator/">https://eurydia.github.io/dsp-calculator/</a>
+				</li>
+				<li>
+					tohalukmanhakim's Dyson Sphere Program Helper (Veins Utilisation &amp; Logistics Carrier Engine stats)
+					<a target="_blank" href="https://tohalukmanhakim.github.io/dyson-sphere-program-helper/">https://tohalukmanhakim.github.io/dyson-sphere-program-helper/</a>
+				</li>
+				<li>
+					Dyson Sphere Planner — Printable recipes
+					<a target="_blank" href="https://dyson-sphere-planner.com/">https://dyson-sphere-planner.com/</a>
+				</li>
+				
+				
+				{/* 
+				Out of date, doesn't work or taken out of curation
+				<li>
+					Migiyubi's Dyson Sphere Program Ingredients Sheet
+					<a target="_blank" href="https://migiyubi.github.io/dsp-balance/">https://migiyubi.github.io/dsp-balance/</a>
+				</li>
+				<li>
+					Luwa's Dyson Sphere Program production tool
+					<a target="_blank" href="https://github.com/luwake/dyson">https://github.com/luwake/dyson</a>
+				</li>
+				<li>
+					Svlik's DSP Mass Production Calculator Tool
+					<a target="_blank" href="https://www.svlik.com/t/dsq/">https://www.svlik.com/t/dsq/</a>
 				</li>
 				<li>
 					AnthorNet's DSP Calculator Production Planner
@@ -37,17 +77,22 @@ export default function Community(props) {
 					<a target="_blank" href="https://yesterdaysun.gitee.io/dsp-calculator/#tab=about">https://yesterdaysun.gitee.io/dsp-calculator/#tab=about</a>
 				</li>
 				<li>
-					lascyb's Circuit Diagram Generator
-					<a target="_blank" href="http://games.bxbon.com/index/dyson_sphere/production">http://games.bxbon.com/index/dyson_sphere/production</a>
-				</li>
-				<li>
-					Svlik's DSP Mass Production Calculator Tool
-					<a target="_blank" href="https://www.svlik.com/t/dsq/">https://www.svlik.com/t/dsq/</a>
-				</li>
-				<li>
 					Petra's Center Brain Archive — Recipe viewer for Dyson Sphere Program
 					<a target="_blank" href="https://gamma-delta.github.io/center-brain-archive/">https://gamma-delta.github.io/center-brain-archive/</a>
 				</li>
+				<li>
+					Dyson Sphere Program Cheat Sheet
+					<a target="_blank" href="https://deniszholob.github.io/dyson-sphere-program-cheat-sheet/">https://deniszholob.github.io/dyson-sphere-program-cheat-sheet/</a>
+				</li>
+				<li>
+					Dyson Sphere Program Dependency Graph
+					<a target="_blank" href="https://olivercardoza.com/dyson-sphere-graphviz/">https://olivercardoza.com/dyson-sphere-graphviz/</a>
+				</li>
+				<li>
+					art0rz's Dyson Sphere Program Calculator
+					<a target="_blank" href="https://art0rz.github.io/dsp-calc/">https://art0rz.github.io/dsp-calc/</a>
+				</li>
+				*/}
 			</ul>
 		</main>
 	);

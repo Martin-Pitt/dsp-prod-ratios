4f69a01bc3d1210e6b22340801fc13c8f726e140
Martin Pitt <martin.r.pitt@gmail.com>
2023-07-31 21:35:23 +0100

    Try to use SPA Github pages redirect, move around redirect logic

--- src/app.jsx
+++ src/app.jsx
@@ -53,55 +53,108 @@ function Root(props) {
 	);
 }
 
-
-function SinglePageAppRedirect() {
-	if(window.location.search.startsWith('?' + encodeURIComponent('[')) && window.location.search.endsWith(encodeURIComponent(']')))
-	{
-		let [pathname, search, hash] = JSON.parse(decodeURIComponent(window.location.search.slice(1)));
-		console.log('Redirect', pathname);
-		return redirect(pathname); // + '?' + search + '#' + hash);
-	}
+function Page(props) {
+	useEffect(() => {
+		document.title = props.title
+	}, [props.title]);
 	
-	// If we are a user already accustomed to the app, then swap default page on first session to a useful one
-	else if(('sessionStorage' in window && 'localStorage' in window) && !sessionStorage.visit)
-	{
-		sessionStorage.visit = true;
+	useEffect(() => {
+		// https://developers.google.com/search/docs/crawling-indexing/javascript/javascript-seo-basics#properly-inject-canonical-links
+		let link = document.querySelector('link[rel="canonical"]');
+		if(link) link.remove();
 		
-		if(state.research.value.length)
-		{
-			// TODO: Either calculator or references, should find out which they seem to use most frequently?
-			return redirect('/calculator');
-		}
-	}
+		link = document.createElement('link');
+		link.setAttribute('rel', 'canonical');
+		link.setAttribute('href', props.canonical);
+		document.querySelector('title').insertAdjacentElement('afterend', link);
+	}, [props.canonical]);
 	
-	// return redirect('/calculator');
-	// return redirect('/intro');
-	return null;
+	useEffect(() => {
+		let meta = document.head.querySelector('meta[name="description"]');
+		if(meta) meta.remove();
+		
+		meta = document.createElement('meta');
+		meta.setAttribute('name', 'description');
+		meta.setAttribute('content', props.description);
+		document.querySelector('title').insertAdjacentElement('afterend', meta);
+	}, [props.description]);
+	
+	return props.element;
 }
-
 
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<Root/>}>
-			
-			<Route path="calculator" element={<Calculator/>}/>
-			<Route path="research" element={<Research/>}/>
-			<Route path="reference" element={<Reference/>}>
+			<Route path="calculator" element={
+				<Page
+					title="Calculator | DSP Ratios"
+					description="Calculator for planning production chains in Dyson Sphere Program"
+					canonical="https://dsp-ratios.com/calculator"
+					element={<Calculator/>}
+				/>
+			}/>
+			<Route path="research" element={
+				<Page
+					title="Research | DSP Ratios"
+					description="Research tree of Dyson Sphere Program, finetune the calculator to your current tech progress"
+					canonical="https://dsp-ratios.com/research"
+					element={<Research/>}
+				/>
+			}/>
+			<Route path="reference" element={
+				<Page
+					title="Reference Sheets | DSP Ratios"
+					description="Quick reference sheets to Dyson Sphere Program such as items, recipes, buildings and technology"
+					canonical="https://dsp-ratios.com/reference"
+					element={<Reference/>}
+				/>
+			}>
 				<Route path="assemble" element={<ReferenceAssemble/>}/>
 				<Route path="smelt" element={<ReferenceSmelt/>}/>
 				<Route path="" loader={() => redirect('assemble')}/>
 			</Route>
-			<Route path="settings" element={<Settings/>}/>
-			{/* <Route path="" element={<Intro/>}/> */}
-			<Route path="intro" loader={() => redirect('/')}/>
-			<Route path="" loader={SinglePageAppRedirect} element={<Intro/>}/>
+			<Route path="settings" element={
+				<Page
+					title="Settings | DSP Ratios"
+					description="Settings for DSP Ratios"
+					canonical="https://dsp-ratios.com/settings"
+					element={<Settings/>}
+				/>
+			}/>
+			<Route path="" element={
+				<Page
+					title="DSP Ratios"
+					description="Plan and design production lines in Dyson Sphere Program with calculator, research and reference sheets"
+					canonical="https://dsp-ratios.com"
+					element={<Intro/>}
+				/>
+			}/>
 		</Route>
 	)
 );
 
 
 export function App(props) {
+	useEffect(() => {
+		let { pathname, search, hash } = location;
+		if(!('sessionStorage' in window && 'localStorage' in window)) return;
+		if(sessionStorage.visit) return;
+		
+		// This is the first session
+		sessionStorage.visit = true;
+		
+		// Did we arrive on the default intro page?
+		if(!(pathname === '/' && search === '' && hash === '')) return;
+		
+		// If we are a user already accustomed to the app, then swap to a useful page instead
+		if(state.research.value.length)
+		{
+			// TODO: Either calculator or references, should find out which they seem to use most frequently?
+			location.replace('/calculator');
+		}
+	}, []);
+	
 	return <RouterProvider router={router} />;
 };
 

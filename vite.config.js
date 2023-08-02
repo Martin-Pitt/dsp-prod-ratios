5c12d34c0e4c8001f8208a38f3c4d7a08d2c7fb9
Martin Pitt <martin.r.pitt@gmail.com>
2023-08-02 07:05:54 +0100

    Prep work towards an MPA potentially

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
+
+
 export default defineConfig({
 	plugins: [preact()],
 	base: '/',
+	build: {
+		// https://vitejs.dev/guide/build.html#multi-page-app
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, 'index.html'),
+			}
+		}
+	}
 });

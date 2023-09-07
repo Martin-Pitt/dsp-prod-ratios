d7c30f8c59fed31c8eaaf2fc6b85342b178e32e2
Martin Pitt <martin.r.pitt@gmail.com>
2023-09-07 19:59:39 +0100

    Hotfix firefox support

--- src/css/variables.js
+++ src/css/variables.js
@@ -2,19 +2,20 @@ let CSSVariables = {};
 
 for(let sheet of document.styleSheets)
 {
-	try { sheet.rules } catch(e) { continue }
+	try { sheet.cssRules } catch(e) { continue }
 	
-	for(let rule of sheet.rules)
+	for(let rule of sheet.cssRules)
 	{
 		if(rule.selectorText !== ':root') continue;
-		let entries = rule.styleMap.entries();
-		entries = Array
-			.from(entries, ([key, value]) => [key, value.toString()])
-			.filter(([key]) => key.startsWith('--'))
-			.map(([key, value]) => [
-				key.substr(2).replace(/-([a-z])/ig, (all, letter) => letter.toUpperCase()),
-				value
-			]);
+		let entries = [];
+		for(let key of rule.style)
+		{
+			let value = rule.style.getPropertyValue(key);
+			if(!key.startsWith('--')) continue;
+			key = key.substr(2).replace(/-([a-z])/ig, (all, letter) => letter.toUpperCase());
+			entries.push([key, value]);
+		}
+		
 		let vars = Object.fromEntries(entries);
 		Object.assign(CSSVariables, vars);
 	}

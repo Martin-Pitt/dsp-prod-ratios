b95475b122de91f4a952e461924ec209f3a93e84
Martin Pitt <martin.r.pitt@gmail.com>
2023-10-01 02:07:19 +0100

    Add zoom levels to the research grid

--- src/components/scrollable-grid.jsx
+++ src/components/scrollable-grid.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect, useCallback, useRef } from 'preact/hooks';
 
+const ZoomLevels = [4, 3, 2, 1.5, 1.25, 1, 1/1.25, 1/1.5, 1/2, 1/3, 1/4]
+const DefaultZoomLevel = ZoomLevels.findIndex(level => level === 1);
+
 
 export default function ScrollableGrid(props) {
 	const Tag = props.tag || 'div';
@@ -60,6 +63,56 @@ export default function ScrollableGrid(props) {
 		setMouseDown(true);
 	}, [root.current, setMouseDown, setPos]);
 	
+	
+	const [zoomLevel, setZoomLevel] = useState(`${name}ZoomLevel` in sessionStorage? parseInt(sessionStorage.getItem(`${name}ZoomLevel`), 10) : DefaultZoomLevel);
+	const zoom = ZoomLevels[zoomLevel];
+	
+	const zoomOut = useCallback(() => {
+		setZoomLevel(oldZoomLevel => {
+			if(oldZoomLevel === ZoomLevels.length - 1) return oldZoomLevel;
+			const newZoomLevel = oldZoomLevel + 1;
+			let newZoom = ZoomLevels[newZoomLevel];
+			let oldZoom = ZoomLevels[oldZoomLevel];
+			let change = 1 / (oldZoom / newZoom);
+			
+			let oldLeft = root.current.scrollLeft + root.current.offsetWidth * 0.5;
+			let oldTop = root.current.scrollTop + root.current.offsetHeight * 0.5;
+			
+			let newLeft = oldLeft * change;
+			let newTop = oldTop * change;
+			
+			root.current.scrollLeft += newLeft - oldLeft;
+			root.current.scrollTop += newTop - oldTop;
+			
+			sessionStorage.setItem(`${name}ZoomLevel`, newZoomLevel);
+			
+			return newZoomLevel;
+		});
+	}, []);
+	const zoomIn = useCallback(() => {
+		setZoomLevel(oldZoomLevel => {
+			if(oldZoomLevel === 0) return oldZoomLevel;
+			const newZoomLevel = oldZoomLevel - 1;
+			let newZoom = ZoomLevels[newZoomLevel];
+			let oldZoom = ZoomLevels[oldZoomLevel];
+			let change = 1 / (oldZoom / newZoom);
+			
+			let oldLeft = root.current.scrollLeft + root.current.offsetWidth * 0.5;
+			let oldTop = root.current.scrollTop + root.current.offsetHeight * 0.5;
+			
+			let newLeft = oldLeft * change;
+			let newTop = oldTop * change;
+			
+			root.current.scrollLeft += newLeft - oldLeft;
+			root.current.scrollTop += newTop - oldTop;
+			
+			sessionStorage.setItem(`${name}ZoomLevel`, newZoomLevel);
+			
+			return newZoomLevel;
+		});
+	}, []);
+	
+	
 	return (
 		<Tag
 			class={props.class}
@@ -69,7 +122,12 @@ export default function ScrollableGrid(props) {
 			onScroll={saveScroll}
 			ref={root}
 		>
-			{props.children}
+			<div style={{zoom}}>{props.children}</div>
+			<div class="zoom">
+				<button title="Make research smaller" onClick={zoomOut}>-</button>
+				<output>{(Math.round(zoom * 1000) / 10)}%</output>
+				<button title="Make research bigger" onClick={zoomIn}>+</button>
+			</div>
 		</Tag>
 	);
 }

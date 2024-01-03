b3fc565048bb5ae33cee35f19a5aa40f62847f9a
Martin Pitt <martin.r.pitt@gmail.com>
2024-01-03 06:20:57 +0000

    Updated to 0.10.28.21172 and see how it works with the new game data format from the parser

--- src/data/reviver.js
+++ src/data/reviver.js
@@ -1,11 +1,11 @@
 // Revive the JSON data
 // usage: const Items = JSON.parse(json, JSONReviver);
 export function JSONReviver(key, value) {
-	if(typeof value === 'object' && typeof value.type === 'string' && value.value)
+	if(value && typeof value === 'object' && typeof value[':type'] === 'string' && value.value)
 	{
-		if(value.type === 'BigInt')
+		if(value[':type'] === 'BigInt')
 			return BigInt(value.value);
-		if(value.type === 'Map')
+		if(value[':type'] === 'Map')
 			return new Map(value.value);
 		else
 			return  value;
@@ -17,9 +17,9 @@ export function JSONReviver(key, value) {
 // Replacer implementation used for stringifying JSON data originally
 export function JSONReplacer(key, value) {
 	if(typeof value === 'bigint')
-		return { type: 'BigInt', value: value.toString() };
+		return { ':type': 'BigInt', value: value.toString() };
 	if(value instanceof Map)
-		return { type: 'Map', value: Array.from(value.entries()) };
+		return { ':type': 'Map', value: Array.from(value.entries()) };
 	
 	else return value;
 }
